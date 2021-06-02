6d1eb7c12537075e0d20aafa8e717a08cf65eb30
EreminMaxim <eremin1988ma21@gmail.com>
2021-06-02 22:11:42 +0300

    git v11 "проверка deploy node.js (const PORT = process.env.PORT || 9999)"

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static("build"));
+app.use(express.static("public"));
 
 const cors = require('cors')
 /********************************************************************/
